3dbfb866e2c7fb188b4c997f27f14683b0ec7388
Jun Rey Ellezo <junreyellezo.cpe@gmail.com>
2021-02-28 04:35:28 +0800

    feat: another one

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,8 @@
 module.exports = {
   purge: [
-    './src/**/*.{js,ts,jsx,tsx}',
+    './src/pages/**/*.{js,ts,jsx,tsx}',
+    './src/views/**/*.{js,ts,jsx,tsx}',
+    './src/components/**/*.{js,ts,jsx,tsx}',
   ],
   darkMode: false, // or 'media' or 'class'
   theme: {
