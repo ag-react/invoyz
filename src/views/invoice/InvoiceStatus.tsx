8b1f031afa5678ea0c2a61407d7ff5fa3535678a
Jun Rey Ellezo <junreyellezo.cpe@gmail.com>
2021-02-28 04:32:00 +0800

    feat: resolve purge issue

--- src/views/invoice/InvoiceStatus.tsx
+++ src/views/invoice/InvoiceStatus.tsx
@@ -2,57 +2,43 @@ import React from 'react';
 
 import { InvoiceStatusEnum } from '../../types/InvoiceTypes';
 
-type TStatusClass = {
-  bg: string,
-  fg: string,
-}
-const kInitialStatusClass: TStatusClass = {
-  bg: '',
-  fg: ''
-}
-
 interface ComponentProps {
   status: InvoiceStatusEnum,
 }
 
 function InvoiceStatus(props: ComponentProps) {
-  const [statusClass, setStatusClass] = React.useState<TStatusClass>(kInitialStatusClass);
-
-  React.useEffect(() => {
-    switch (props.status) {
-      case InvoiceStatusEnum.DRAFT:
-        setStatusClass({
-          bg: "dim-faded",
-          fg: "dim"
-        });
-        break;
-
-      case InvoiceStatusEnum.PENDING:
-        setStatusClass({
-          bg: "yellow-faded",
-          fg: "yellow"
-        });
-        break;
-
-      case InvoiceStatusEnum.PAID:
-        setStatusClass({
-          bg: "green-faded",
-          fg: "green"
-        });
-        break;
-
-      default:
-        break;
-    }
-  }, [props.status])
-
   return (
-    <div className={`rounded-md w-full h-10 capitalize text-h4 font-bold flex items-center justify-center space-x-2 bg-${statusClass.bg} text-${statusClass.fg}`}>
-      <div className={`rounded-full w-2 h-2 bg-${statusClass.fg}`}></div>
-      <span>
-        { props.status }
-      </span>
-    </div>
+    <>
+      {
+        props.status === InvoiceStatusEnum.DRAFT &&
+        <div className={`rounded-md w-full h-10 capitalize text-h4 font-bold flex items-center justify-center space-x-2 bg-dim-faded text-dim`}>
+          <div className={`rounded-full w-2 h-2 bg-dim`}></div>
+          <span>
+            { props.status }
+          </span>
+        </div>
+      }
+
+      {
+        props.status === InvoiceStatusEnum.PENDING &&
+        <div className={`rounded-md w-full h-10 capitalize text-h4 font-bold flex items-center justify-center space-x-2 bg-yellow-faded text-yellow`}>
+          <div className={`rounded-full w-2 h-2 bg-yellow`}></div>
+          <span>
+            { props.status }
+          </span>
+        </div>
+      }
+
+      {
+        props.status === InvoiceStatusEnum.PAID &&
+        <div className={`rounded-md w-full h-10 capitalize text-h4 font-bold flex items-center justify-center space-x-2 bg-green-faded text-green`}>
+          <div className={`rounded-full w-2 h-2 bg-green`}></div>
+          <span>
+            { props.status }
+          </span>
+        </div>
+      }
+    </>
   )
 }
 
