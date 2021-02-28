9a8c4fe2606f96275d08b2345bdb79e0389a99ba
Jun Rey Ellezo <junreyellezo.cpe@gmail.com>
2021-02-28 20:01:00 +0800

    feat:analyzing build issue

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,10 +11,10 @@ function Home() {
   return (
     <div className="flex flex-col items-center py-18 px-6 md:px-12">
       <div className="max-w-3xl w-full">
-        {/* {
-          invoiceStore.currentInvoices &&
+        {
+          invoiceStore.currentInvoices.length > 0 &&
           <InvoiceList invoices={invoiceStore.currentInvoices} />
-        } */}
+        }
       </div>
     </div>
   )
