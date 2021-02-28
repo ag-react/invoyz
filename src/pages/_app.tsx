a51e4798c85224673ac77b47f62e8343a9d17537
Jun Rey Ellezo <junreyellezo.cpe@gmail.com>
2021-02-28 19:40:52 +0800

    feat: update tailwindcss versions

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import Head from 'next/head';
 
+import "tailwindcss/tailwind.css";
+
 import '../styles/globals.scss';
 
 import { StoreProvider } from '../contexts/store-context';
