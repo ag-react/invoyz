4d4ba44594be76e31f68b02b9a5cf8b87055b224
Jun Rey Ellezo <junreyellezo.cpe@gmail.com>
2021-03-03 21:00:02 +0800

    feat: update section tag to div

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -18,13 +18,13 @@ function MyApp({ Component, pageProps }) {
 
     <StoreProvider >
       <div className="relative flex h-screen bg-light dark:bg-dark">
-        <section className="absolute z-9999 left-0 top-0 right-0 lg:right-auto bottom-auto lg:bottom-0 h-18 md:h-20 lg:h-auto w-full lg:w-20">
+        <div className="absolute z-9999 left-0 top-0 right-0 lg:right-auto bottom-auto lg:bottom-0 h-18 md:h-20 lg:h-auto w-full lg:w-20">
           <MenuView />
-        </section>
+        </div>
 
-        <section className="pl-0 mt-18 md:mt-20 lg:pl-20 lg:mt-0 overflow-y-auto w-full">
+        <div className="pl-0 mt-18 md:mt-20 lg:pl-20 lg:mt-0 overflow-y-auto w-full">
           <Component {...pageProps} />
-        </section>
+        </div>
       </div>
     </StoreProvider>
   </>;
